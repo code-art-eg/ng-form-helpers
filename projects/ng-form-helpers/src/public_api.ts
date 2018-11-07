c566fbc118e63964d334458a843260e985659072
Sherif Elmetainy <sherif.elmetainy@code-art.net>
2018-11-07 12:47:59 +0200

    include form models in public api

--- projects/ng-form-helpers/src/public_api.ts
+++ projects/ng-form-helpers/src/public_api.ts
@@ -6,3 +6,7 @@ export * from './lib/ng-form-helpers.module';
 export * from './lib/subject-wrapper';
 export * from './lib/directives/base-converter-directive';
 
+export * from './lib/form-models';
+export * from './lib/typed-form-array';
+export * from './lib/typed-form-group';
+export * from './lib/typed-form-control';
