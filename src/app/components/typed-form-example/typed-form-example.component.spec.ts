3994be167564dd3a7e48d538d7cd01692af0d7c6
Sherif Elmetainy <sherif.elmetainy@gmail.com>
2020-09-13 22:27:32 +0200

    Bump angular version to 10

--- src/app/components/typed-form-example/typed-form-example.component.spec.ts
+++ src/app/components/typed-form-example/typed-form-example.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TypedFormExampleComponent } from './typed-form-example.component';
 
@@ -6,7 +6,7 @@ describe('TypedFormExampleComponent', () => {
   let component: TypedFormExampleComponent;
   let fixture: ComponentFixture<TypedFormExampleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [TypedFormExampleComponent]
     })
